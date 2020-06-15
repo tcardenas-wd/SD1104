a6e2735bc835b7d799e3857bd355498bb8b2d0da
tcardenas-wd <66964497+tcardenas-wd@users.noreply.github.com>
2020-06-15 14:17:01 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myFirstNumber = 19;
+            int mySecondNumber = 244;
+            Console.WriteLine(myFirstNumber * mySecondNumber);
+
+            string myText = "Some Text";
+            Console.WriteLine(myText + mySecondNumber);
+
+            string moreText = "Goes Here";
+            Console.WriteLine(myText + moreText);
+
+            string color1 = "Yellow";
+            string color2 = "Red";
+            bool isYellow = true;
+            bool isGreen = false;
+            Console.WriteLine(color1 + isYellow + color2 + isGreen);
+
+            double numbers;
+            numbers = mySecondNumber / myFirstNumber;
+            Console.WriteLine(numbers);
+
+            numbers = mySecondNumber / (double)myFirstNumber;
+            Console.WriteLine(numbers);
+
+            Console.WriteLine("Breakpoint Here");
+
+            string textToNumber = "80473";
+            int results = 99 * int.Parse(textToNumber);
+            Console.WriteLine(results);
+
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine(rightNow);
+        }
+    }
+}
