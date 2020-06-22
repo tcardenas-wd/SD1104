dcc76c6b22afa2fa0719672cded13ffd55fd8907
tcardenas-wd <66964497+tcardenas-wd@users.noreply.github.com>
2020-06-21 23:11:13 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+
+            Console.WriteLine("Enter a value for the first number");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a value for the second number");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = number1 + number2;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(result);
+
+            result = number1 - number2;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(result);
+
+            result = number1 * number2;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(result);
+
+            result = number1 / number2;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(result);
+
+            string name;
+
+            Console.WriteLine("Enter name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            int digit1;
+            int digit2;
+            int digit3;
+
+            Console.WriteLine("Enter a value for the first number");
+            digit1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a value for the second number");
+            digit2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a value for the third number");
+            digit3 = Convert.ToInt32(Console.ReadLine());
+
+            result = digit1 * digit2 * digit3;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(result);
+
+            string age;
+
+            Console.WriteLine("Enter age");
+            age = Console.ReadLine();
+
+            Console.WriteLine("You look younger than " + age);
+          
+        }
+    }
+}
